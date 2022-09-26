6f84cb2bd9a849ef4553a07819ead0bdc32639ba
Vahid <vahid.pakdaman1209@gmail.com>
2022-09-26 12:45:48 +0330

    Create ICommentRepository

---
+++ Domain/CommentAgg/ICommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.UserAgg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.CommentAgg
+{
+	public interface ICommentRepository : IRepository<Guid, Comment>
+	{
+		Task<List<Comment>> GetByUserId(Guid userId);
+	}
+}
