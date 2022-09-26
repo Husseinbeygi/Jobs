24f07b66ac5bac44b0eeb35cf7b7a13c8d2656e0
Vahid <vahid.pakdaman1209@gmail.com>
2022-09-26 12:52:20 +0330

    Add Comments property to DatabaseContext

--- Persistence/DatabaseContext.cs
+++ Persistence/DatabaseContext.cs
@@ -6,6 +6,7 @@ namespace Persistence
 	public class DatabaseContext : DbContext
 	{
 		public DbSet<User> Users { get; set; }
+		public DbSet<Comment> Comments { get; set; }
 		public DatabaseContext(DbContextOptions options) : base(options)
 		{
 			Database.EnsureCreated();	
