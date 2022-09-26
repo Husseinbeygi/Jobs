d82f68db9802d997e40123cb67fd40966e169998
Vahid <vahid.pakdaman1209@gmail.com>
2022-09-26 13:15:23 +0330

    Add CommentRepository to Repositories

---
+++ Persistence/Repositories/CommentRepository.cs
@@ -0,0 +1,27 @@
+﻿using Domain.CommentAgg;
+using Domain.UserAgg;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+	public class CommentRepository : RepositoryBase<Guid, Comment>, ICommentRepository
+	{
+		private readonly DatabaseContext _context;
+		public CommentRepository(DatabaseContext context) : base(context)
+		{
+			_context = context;
+		}
+
+		public async Task<List<Comment>> GetByUserId(Guid userId)
+		{
+			
+			return await _context.Comments.Where(x => x.UserId == userId).ToListAsync();
+
+		}
+	}
+}
