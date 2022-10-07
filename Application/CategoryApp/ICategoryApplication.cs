f4011169acc06ea9233a9c484734ad0bc6f6dab0
Sajjad Doosty <sajjad.doosty.83@gmail.com>
2022-10-07 23:01:15 +0330

    ساخت فابل های اپلیکیشن مرتبط با کتگوری

---
+++ Application/CategoryApp/ICategoryApplication.cs
@@ -0,0 +1,18 @@
+﻿using Domain.CategoryAgg;
+using Framework.OperationResult;
+using ViewModels.Pages.Admin.Categories;
+
+namespace Application.CategoryApp
+{
+    public interface ICategoryApplication
+    {
+        Task<OperationResult> AddCategory(CommonViewModel category);
+        Task<OperationResult> UpdateCategory(UpdateViewModel category);
+        Task<OperationResult> DeleteCategory(Guid Id);
+        Task<OperationResultWithData<List<Category>>> GetAllCategories();
+        Task<OperationResultWithData<DetailsViewModel>> GetCategory(Guid Id);
+        Task<OperationResultWithData<Category>> GetCategoryByName(string categoryName);
+        Task<OperationResultWithData<List<Category>>> GetParentCategories();
+        Task<OperationResultWithData<Dictionary<IndexViewModel, List<IndexViewModel>>>> GetGroupedCategories();
+    }
+}
