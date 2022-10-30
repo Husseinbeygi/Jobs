778652b6e2053b20eb7ccbdf0314450415ffcb06
MohammadSadeghAb <113609836+MohammadSadeghAb@users.noreply.github.com>
2022-10-30 21:32:17 +0330

    درست کردن insertdatetime و updatedatetime

--- Server/Pages/Admin/Jobs/Index.cshtml.cs
+++ Server/Pages/Admin/Jobs/Index.cshtml.cs
@@ -3,10 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using ViewModels.Pages.Admin.Job;
 
-namespace Server.Pages.Admin.Jobs
-{
+namespace Server.Pages.Admin.Jobs;
+
+    [Microsoft.AspNetCore.Authorization.Authorize
+	    (Roles = Infrastructure.Constants.Role.Admin)]
+
     public class IndexModel : Infrastructure.BasePageModel
     {
         public IndexModel(ILogger<IndexModel> logger, IJobApplication jobApplication)
@@ -20,9 +24,8 @@ namespace Server.Pages.Admin.Jobs
         public IJobApplication JobApplication { get; set; }
         public IList<DetailsViewModel> ViewModel { get; set; }
 
-        public async void OnGetAsync()
+        public async Task OnGetAsync()
         {
             ViewModel = (await JobApplication.GetAllJob()).Data;
         }
     }
-}
