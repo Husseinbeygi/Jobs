8f1eed5fa37f7ce63a88511e81c4cb202751a75f
Sajjad Doosty <sajjad.doosty.83@gmail.com>
2022-12-11 01:21:49 +0330

    کامل کردن بخش رجیستر کاربر

--- Server/Pages/Account/Register.cshtml.cs
+++ Server/Pages/Account/Register.cshtml.cs
@@ -1,34 +1,51 @@
-﻿using System.Linq;
-using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
+﻿using Application.AccountApp;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using ViewModels.Pages.Account;
 
 namespace Server.Pages.Security
 {
-	public class RegisterModel : Infrastructure.BasePageModel
-	{
-		public RegisterModel(ILogger<RegisterModel> logger) 
-		{
-			Logger = logger;
-			ViewModel = new();
-		}
+    public class RegisterModel : BasePageModel
+    {
+        private readonly IAccountApplication _accountApplication;
 
-		// **********
-		[Microsoft.AspNetCore.Mvc.BindProperty]
-		public ViewModels.Pages.Account.RegisterViewModel ViewModel { get; set; }
-		// **********
+        public RegisterModel(IAccountApplication accountApplication)
+        {
+            _accountApplication = accountApplication;
+            ViewModel = new();
+        }
 
-		// **********
-		private Microsoft.Extensions.Logging.ILogger<RegisterModel> Logger { get; }
-		// **********
 
-		public void OnGet()
-		{
-		}
+        // **********
+        [BindProperty]
+        public RegisterViewModel ViewModel { get; set; }
+        // **********
 
-		public async Task OnPostAsync()
-		{
+        public void OnGet()
+        {
+        }
 
-		}
-	}
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid == false)
+            {
+                return Page();
+            }
+
+            var res = await _accountApplication.RegisterUser(ViewModel);
+
+            if (res.Succeeded == false || 0 < res.ErrorMessages.Count)
+            {
+                foreach (var error in res.ErrorMessages)
+                {
+                    AddPageError(error);
+                }
+
+                return Page();
+            }
+
+            return RedirectToPage("Login");
+        }
+    }
 }
