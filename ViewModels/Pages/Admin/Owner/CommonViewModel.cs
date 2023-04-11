8b72e7b8571935105859344af83424c234352c1e
MohammadSadeghAb <113609836+MohammadSadeghAb@users.noreply.github.com>
2023-04-11 17:17:20 +0430

    ساخت ویو مدل Owner

---
+++ ViewModels/Pages/Admin/Owner/CommonViewModel.cs
@@ -0,0 +1,87 @@
+﻿using Domain.SeedWork;
+using Resources.Messages;
+using System.ComponentModel.DataAnnotations;
+
+namespace ViewModels.Pages.Admin.Owner
+{
+    public class CommonViewModel
+    {
+        // **********
+        [MaxLength
+        (length: Constants.MaxLength.FullName,
+        ErrorMessageResourceType = typeof(Validations),
+        ErrorMessageResourceName = nameof(Validations.MaxLength))]
+
+        [Display
+        (ResourceType = typeof(Resources.DataDictionary),
+        Name = nameof(Resources.DataDictionary.FullName))]
+
+        public string FName { get; set; }
+        // **********
+
+        // **********
+        [MaxLength
+        (length: Constants.MaxLength.LastName,
+        ErrorMessageResourceType = typeof(Validations),
+        ErrorMessageResourceName = nameof(Validations.MaxLength))]
+
+        [Display
+        (ResourceType = typeof(Resources.DataDictionary),
+        Name = nameof(Resources.DataDictionary.LastName))]
+
+        public string LName { get; set; }
+        // **********
+
+        // **********
+        [MaxLength
+        (length: Constants.FixedLength.CellPhoneNumber,
+        ErrorMessageResourceType = typeof(Validations),
+        ErrorMessageResourceName = nameof(Validations.MaxLength))]
+
+        [Display
+        (ResourceType = typeof(Resources.DataDictionary),
+        Name = nameof(Resources.DataDictionary.CellPhoneNumber))]
+
+        public string? PhoneNumber { get; set; }
+        // **********
+
+        // **********
+        [Display
+        (ResourceType = typeof(Resources.DataDictionary),
+        Name = nameof(Resources.DataDictionary.Jobs))]
+
+        public List<Guid> JobId { get; set; }
+        // **********
+
+        // **********
+        [Display
+        (ResourceType = typeof(Resources.DataDictionary),
+        Name = nameof(Resources.DataDictionary.IsActive))]
+
+        public bool IsActive { get; set; }
+        // **********
+
+        // **********
+        [Display
+        (ResourceType = typeof(Resources.DataDictionary),
+        Name = nameof(Resources.DataDictionary.IsSystemic))]
+        public bool IsSystemic { get; set; }
+        // **********
+
+        // **********
+        [Display
+        (ResourceType = typeof(Resources.DataDictionary),
+        Name = nameof(Resources.DataDictionary.IsUndeletable))]
+
+        public bool IsUndeletable { get; set; }
+        // **********
+
+        // **********
+        [Display
+        (ResourceType = typeof(Resources.DataDictionary),
+        Name = nameof(Resources.DataDictionary.UpdateDateTime))]
+
+        public DateTime UpdateDateTime { get; private set; }
+        // **********
+    }
+}
