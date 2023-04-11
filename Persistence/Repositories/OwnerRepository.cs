d2bbd790cad276da771c89698f4521e5fb04f2b1
MohammadSadeghAb <113609836+MohammadSadeghAb@users.noreply.github.com>
2023-04-11 17:12:47 +0430

    ساخت IOwnerRepository و OwnerRepository

---
+++ Persistence/Repositories/OwnerRepository.cs
@@ -0,0 +1,30 @@
+﻿using Domain.OwnerAgg;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class OwnerRepository : RepositoryBase<Guid, Owner>, IOwnerRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public OwnerRepository(DatabaseContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<Owner> GetByName(string lname)
+        {
+            return await _context.Owners.FirstOrDefaultAsync(x => x.LName == lname);
+        }
+
+        public async Task<IList<Owner>> GetAllAsync()
+        {
+            return await _context.Owners.ToListAsync();
+        }
+    }
+}
